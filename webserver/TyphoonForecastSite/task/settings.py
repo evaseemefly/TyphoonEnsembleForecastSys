0f52af0d78473d79b20e108655c8b1649d37f880
evaseemeflt-t7920 <evaseemefly@126.com>
2021-08-30 16:43:25 +0800

    title:21-08-30 暂时加入 task

---
+++ webserver/TyphoonForecastSite/task/settings.py
@@ -0,0 +1,14 @@
+# celery 相关配置
+
+# 使用RabbitMQ作为消息代理
+BROKER_URL = f'amqp://admin:admin123@localhost:5672/'
+# 把任务结果存在了Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# 任务序列化和反序列化使用JSON方案
+CELERY_TASK_SERIALIZER = 'pickle'
+# 读取任务结果使用JSON
+CELERY_RESULT_SERIALIZER = 'json'
+# 任务过期时间，不建议直接写86400，应该让这样的magic数字表述更明显
+CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24
+# 指定接受的内容类型，是个数组，可以写多个
+CELERY_ACCEPT_CONTENT = ['json', 'pickle']
---
+++ webserver/TyphoonForecastSite/task/tasks.py
@@ -0,0 +1,30 @@
+import os
+from celery import Celery, platforms
+from django.conf import settings
+from task.settings import BROKER_URL, CELERY_RESULT_BACKEND, CELERY_TASK_SERIALIZER, CELERY_RESULT_SERIALIZER, \
+    CELERY_TASK_RESULT_EXPIRES, CELERY_ACCEPT_CONTENT
+
+# 为celery设置环境变量 django -> settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TyphoonForecastSite.settings')
+app = Celery(
+    backend='amqp',
+    broker=BROKER_URL,
+    CELERY_ROUTES={
+        'worker.test1': {'queue': 'test1'}
+    },
+)
+
+# TODO:[*] celery 3.1 开始可以通过如下的方式获取task的id
+@app.task(bind=True)
+def my_task(self, msg):
+    print('测试耗时任务')
+    print(f'传入的参数为:{msg}')
+    print(f'{self}')
+    if hasattr(msg, 'username'):
+        print(f'self.request.id:{self.request.id}|{msg.username}')
+    print(f'开始调用oil job')
+    # print(sys.path)
+    # main.do_job()
+    # time.sleep(5)
+    print('耗时任务结束')
+    print('--------------')
