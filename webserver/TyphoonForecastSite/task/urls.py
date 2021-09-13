44bbc642eea9555d19ca61915cc4efe6da1cbfc9
evaseemefly by 7530 <evaseemefly@126.com>
2021-09-13 22:05:51 +0800

    title: 21-09-13 完成获取最近的 10个 task的进度

--- webserver/TyphoonForecastSite/task/urls.py
+++ webserver/TyphoonForecastSite/task/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 
 # 本项目
-from .views import TestView, TaskRateView
+from .views import TestView, TaskRateView, TaskRecentNumListView
 
 app_name = '[task]'
 
@@ -11,5 +11,6 @@ urlpatterns = [
     # 根据查询条件获取 typhoonDetailModel 的列表
     url(r'^todo$', TestView.as_view()),
     url(r'^last/rate$', TaskRateView.as_view()),  # 获取指定任务的最后的进度
-
+    url(r'^last/num/list$', TaskRecentNumListView.as_view()),  # 获取最后的几个任务的进度
+    url(r'^last/date/list$', TaskRecentNumListView.as_view())
 ]
--- webserver/TyphoonForecastSite/task/views.py
+++ webserver/TyphoonForecastSite/task/views.py
@@ -25,6 +25,7 @@ class TaskRateView(BaseView):
     """
         获取指定 celery_id 的对应进度的最后的进度
     """
+
     def get(self, request: Request) -> Response:
         celery_id_str: str = request.GET.get('celery_id', UNLESS_CELERY_ID)
         # celery_id: int = celery_id_str if celery_id_str is not None else UNLESS_CELERY_ID
@@ -42,3 +43,49 @@ class TaskRateView(BaseView):
         except Exception as ex:
             self.json_data = ex.args
         return Response(self.json_data, self._status)
+
+
+class TaskRecentNumListView(BaseView):
+    """
+        获取最近的 num 个 task 作业进度列表
+    """
+    num: int = 10
+    max_num: int = 20
+
+    def get(self, request: Request) -> Response:
+        num_str: str = request.GET.get('num', None)
+        num: int = int(num_str) if num_str is not None else self.num
+        if num > self.max_num:
+            num = self.max_num
+        rate_list = []
+        try:
+            # list_celery_ids = []
+            list_celery_ids = list(CaseStatusModel.objects.values('celery_id').distinct())[-num:]
+            if len(list_celery_ids) == 0:
+                raise QueryNoneError()
+            for temp_celery in list_celery_ids:
+                temp_celery_id: str = temp_celery.get('celery_id')
+                temp_task: CaseStatusModel = CaseStatusModel.objects.filter(celery_id=temp_celery_id).order_by(
+                    '-case_rate').values('celery_id', 'case_state', 'case_rate', 'gmt_created').first()
+                rate_list.append(temp_task)
+
+            self.json_data = CaseStatusModelSerializer(rate_list, many=True).data
+            self._status = 200
+        except QueryNoneError as qNoneEx:
+            self.json_data = qNoneEx.message
+        except Exception as ex:
+            self.json_data = ex.args
+        return Response(self.json_data, self._status)
+
+    pass
+
+
+class TaskRecentDateStatisticsListView(BaseView):
+    """
+        获取 最近 几日的创建的作业的统计列表
+        step:
+         -1 获取最近 num 的日期列表
+         -2 根据指定日期获取该日期所创建的 task 的 celery_id
+         -3 获取该日的 celery_id 的总数
+    """
+    pass
