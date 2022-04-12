648079e1137de564c9ea7a4241f91c7a85102965
evaseemeflt-t7920 <evaseemefly@126.com>
2022-04-12 16:35:19 +0800

    title:22-04-12 单位 server 提交

--- background/02ByDelayTask/proj/conf/settings.py
+++ background/02ByDelayTask/proj/conf/settings.py
@@ -37,17 +37,17 @@ DATABASES = {
         # mac
         'USER': 'root',  # 账号
         # 7530,mac
-        'PASSWORD': '12345678',
+        # 'PASSWORD': '12345678',
         # 5820,p52s,p500,razer
-        # 'PASSWORD': '123456',
-        # 'HOST': '128.5.10.21',  # HOST
+        'PASSWORD': '123456',
+        'HOST': '128.5.10.21',  # HOST
         # 'HOST': '172.18.0.1',  # HOST
         # 'HOST': '127.0.0.1',  # HOST
         # 访问宿主的mysql服务,
-        'HOST': 'host.docker.internal',   # TODO:[*] 22-01-20 暂时注释掉，由于路径使用win的路径，暂时不使用docker作为解释器
-        'POST': 3306,  # 端口
+        # 'HOST': 'host.docker.internal',   # TODO:[*] 22-01-20 暂时注释掉，由于路径使用win的路径，暂时不使用docker作为解释器
+        # 'POST': 3306,  # 端口
         # 'HOST': 'mysql',  # HOST
-        # 'POST': 3308,  # 端口
+        'POST': 3308,  # 端口
         'OPTIONS': {
             "init_command": "SET foreign_key_checks = 0;",
         },
